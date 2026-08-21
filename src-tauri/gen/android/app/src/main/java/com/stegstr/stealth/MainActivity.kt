4b017e0a22f91222a4b7a697e57b3b4eba524e00
zeeshan ali <shanishah219@gmail.com>
2026-08-21 08:20:45 +0500

    android build fixes

---
+++ src-tauri/gen/android/app/src/main/java/com/stegstr/stealth/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.stegstr.stealth
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
